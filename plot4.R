ea21179d7c679b587f88928ba6dcfb90b8d819f2
JavierQuinteroRamirez <javierquinteroramirez@gmail.com>
2020-09-30 16:43:23 -0500

    Comments

--- plot4.R
+++ plot4.R
@@ -7,7 +7,7 @@
 
 #Let's Make the plot!
 
-#if plot3 already exists, delete it.
+#if plot4 already exists, delete it.
 plot4name = "plot4.png"
 
 if (file.exists(plot4name)) { 
@@ -19,13 +19,15 @@ png(filename = plot4name, width = 480, height = 480)
 
 par(mfrow=c(2, 2))
 
+#Global Active Power plot
 plot(finaldata$Global_active_power, type = "l", xaxt = "n", xlab = "", ylab="Global Active Power")
 axis(1, at = c(1, as.integer(nrow(finaldata)/2), nrow(finaldata)), labels = c("Thu", "Fri", "Sat"))
 
+#Voltage plot
 plot(finaldata$Voltage, type="l",xaxt="n",xlab = "Datetime", ylab = "Voltage")
 axis(1, at = c(1, as.integer(nrow(finaldata)/2), nrow(finaldata)), labels = c("Thu", "Fri", "Sat"))
 
-
+#Energy sub metering plot
 with(finaldata, {
   plot(Sub_metering_1,type = "l", xaxt = "n", xlab ="", ylab="Energy sub metering")
   lines(x=Sub_metering_2, col = "red")
@@ -35,7 +37,8 @@ axis(1, at=c(1, as.integer(nrow(finaldata)/2), nrow(finaldata)), labels = c("Thu
 legend("topright", lty=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        box.lwd = 0, box.col = "transparent", bg="transparent")
 
-plot(finaldata$Global_reactive_power, type ="l", xaxt = "n", xlab = "Datetime", ylab = "Global reactive power")
+#Global Reactive Power plot
+plot(finaldata$Global_reactive_power, type ="l", xaxt = "n", xlab = "Datetime", ylab = "Global Reactive Power")
 axis(1, at = c(1, as.integer(nrow(finaldata)/2), nrow(finaldata)), labels = c("Thu", "Fri", "Sat"))
 
 dev.off()
