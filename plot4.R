77ff136d88efd891fb2914e4d6234b1d17c3ced8
JavierQuinteroRamirez <javierquinteroramirez@gmail.com>
2020-09-30 16:38:48 -0500

    Plot 4 image and code

---
+++ plot4.R
@@ -0,0 +1,41 @@
+#Exploratory Data Analysis - First Course Project
+#Javier Quintero Ramírez
+
+
+#It's mandatory run the plot1.R and plot2.R first !
+
+
+#Let's Make the plot!
+
+#if plot3 already exists, delete it.
+plot4name = "plot4.png"
+
+if (file.exists(plot4name)) { 
+  file.remove(plot4name)
+}
+
+
+png(filename = plot4name, width = 480, height = 480)
+
+par(mfrow=c(2, 2))
+
+plot(finaldata$Global_active_power, type = "l", xaxt = "n", xlab = "", ylab="Global Active Power")
+axis(1, at = c(1, as.integer(nrow(finaldata)/2), nrow(finaldata)), labels = c("Thu", "Fri", "Sat"))
+
+plot(finaldata$Voltage, type="l",xaxt="n",xlab = "Datetime", ylab = "Voltage")
+axis(1, at = c(1, as.integer(nrow(finaldata)/2), nrow(finaldata)), labels = c("Thu", "Fri", "Sat"))
+
+
+with(finaldata, {
+  plot(Sub_metering_1,type = "l", xaxt = "n", xlab ="", ylab="Energy sub metering")
+  lines(x=Sub_metering_2, col = "red")
+  lines(x=Sub_metering_3, col = "blue")
+})
+axis(1, at=c(1, as.integer(nrow(finaldata)/2), nrow(finaldata)), labels = c("Thu", "Fri", "Sat"))
+legend("topright", lty=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       box.lwd = 0, box.col = "transparent", bg="transparent")
+
+plot(finaldata$Global_reactive_power, type ="l", xaxt = "n", xlab = "Datetime", ylab = "Global reactive power")
+axis(1, at = c(1, as.integer(nrow(finaldata)/2), nrow(finaldata)), labels = c("Thu", "Fri", "Sat"))
+
+dev.off()
